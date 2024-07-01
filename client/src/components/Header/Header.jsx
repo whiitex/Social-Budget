289b85e6bb1832fbb282e5235c3764b1436efd7a
whiitex <pasquale03.bianco@gmail.com>
2024-07-01 18:55:42 +0200

    feat: header react component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navbar, Nav, Button } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar className="navbar-expand-lg navbar-dark bg-dark text-white py-2 fixed-top d-flex justify-content-between">
+      <Navbar.Brand className="ml-5 mr-0">Social Budget 2024</Navbar.Brand>
+      <Button id="adminButton">
+        {">>"} next phase {"<<"}
+      </Button>
+      <Button className="btn-info mr-5" type="button" id="loginButton">
+        Login
+      </Button>
+    </Navbar>
+  );
+};
+
+export default Header;
