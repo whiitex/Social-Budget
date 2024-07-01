d8be77eb701618d0e3458f3ba9dd5810a2fc0699
whiitex <pasquale03.bianco@gmail.com>
2024-07-01 19:10:52 +0200

    feat: footer react component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-dark text-white py-2">
+      <Container>
+        <p className="my-0 mb-1">
+          &copy; Social Budget 2024. All Rights Reserved.
+        </p>
+        <p className="my-0 py-0">whiitex</p>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import { Navbar, Button } from "react-bootstrap";
 
 const Header = () => {
   return (
     <Navbar className="navbar-expand-lg navbar-dark bg-dark text-white py-2 fixed-top d-flex justify-content-between">
       <Navbar.Brand className="ml-5 mr-0">Social Budget 2024</Navbar.Brand>
-      <Button id="adminButton">
+      <Button type="button" id="adminButton">
         {">>"} next phase {"<<"}
       </Button>
       <Button className="btn-info mr-5" type="button" id="loginButton">
