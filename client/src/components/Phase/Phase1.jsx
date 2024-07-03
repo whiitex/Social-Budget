578fcef66bc28971545e133cc2f71486f87c8f90
whiitex <pasquale03.bianco@gmail.com>
2024-07-03 19:04:58 +0200

    feat: insert, edit, proposal

--- client/src/components/Phase/Phase1.jsx
+++ client/src/components/Phase/Phase1.jsx
@@ -6,39 +6,76 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import ProposalAPI from "../../API/proposal.api.mjs";
 
 const Phase1 = ({ user }) => {
+  const [shouldRefresh, setShouldRefresh] = useState(false);
   const [proposals, setProposals] = useState([]);
   const [description, setDescription] = useState("");
   const [cost, setCost] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
+    setShouldRefresh(false);
     ProposalAPI.getAllProposals()
       .then((propos) => {
         setErrorMessage("");
         setProposals(propos.filter((p) => p.author === user.username));
       })
       .catch((err) => setErrorMessage(err.message));
-  }, [user]);
+  }, [user, shouldRefresh]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setErrorMessage("");
     const proposal = { description, cost };
     e.target.reset();
     setDescription("");
     setCost(0);
 
     ProposalAPI.insertProposal(proposal)
-      .then(() => setProposals([...proposals, proposal]))
+      .then(() => setShouldRefresh(true))
       .catch((err) => setErrorMessage(err.message));
+  };
+
+  // handle DigitalButtons
+  const onClickButton = (up) => {
+    return (e) => {
+      setErrorMessage("");
+      const prevNumber =
+        parseFloat(
+          e.target.parentElement.parentElement.parentElement.children[0]
+            .children[1].value
+        ) || 0;
+      const newNumber = prevNumber + (up ? 10 : -10);
+      const finalNumber = newNumber >= 0 ? newNumber : 0;
+      e.target.parentElement.parentElement.parentElement.children[0].children[1].value =
+        finalNumber;
+      setCost(finalNumber);
+    };
+  };
+
+  const handleSetCost = (e) => {
+    let flag = true;
+    for (const char of e.target.value) {
+      if (!parseInt(char) && char !== '.' && char !== ',') flag = false;
+    }
+    if (!flag && e.target.value !== "") setErrorMessage("Cost must be a number");
+    else {
+      setErrorMessage("");
+      setCost(parseFloat(e.target.value));
+    }
   };
 
   return (
     <div id="proposals" className="row mb-5">
       {proposals.map((proposal, index) => (
         <Proposal1
-          key={index}
+          key={proposal.id}
+          id={proposal.id}
           cost={proposal.cost}
           description={proposal.description}
+          setShouldRefresh={setShouldRefresh}
+          setErrorMessage={setErrorMessage}
+          setCost={setCost}
+          setDescription={setDescription}
         />
       ))}
 
@@ -71,10 +108,13 @@ const Phase1 = ({ user }) => {
                       className="form-control inputText"
                       id="cost"
                       placeholder="Enter value"
-                      onChange={(e) => setCost(parseFloat(e.target.value))}
+                      onChange={handleSetCost}
                     />
                   </div>
-                  <DigitalButtons margintop="mt-4" />
+                  <DigitalButtons
+                    margintop="mt-4"
+                    onClickButton={onClickButton}
+                  />
                 </div>
               </Form.Group>
               <Form.Group className="mb-3">
