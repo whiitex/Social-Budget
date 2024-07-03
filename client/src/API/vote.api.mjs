a166e7ca828d677fe1aa5cd7e9dd7d0c88755ace
whiitex <pasquale03.bianco@gmail.com>
2024-07-04 00:32:51 +0200

    feat: vote api

---
+++ client/src/API/vote.api.mjs
@@ -0,0 +1,60 @@
+import handleInvalidResponse from "./utils.mjs";
+
+const SERVER_URL = "http://localhost:3001/socialbudget/api/votes";
+
+/**
+ * Get the score of all proposals
+ */
+const getScore = async () => {
+  return await fetch(SERVER_URL + "/", {
+    method: "GET",
+  })
+    .then(handleInvalidResponse)
+    .then((response) => response.json());
+};
+
+/**
+ * Increase the score of a proposal
+ * It requires the user to be logged in.
+ * The proposal id and the rating are sent in the request body.
+ * - id: number
+ * - rating: number
+ */
+const insertVote = async (id, rating) => {
+  return await fetch(SERVER_URL + "/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ id, rating }),
+  })
+    .then(handleInvalidResponse)
+    .then((response) => response.json());
+};
+
+/**
+ * Remove the score of a proposal
+ * It requires the user to be logged in.
+ * The proposal id is sent in the request body.
+ *  - id: number
+ */
+const removeScore = async (id) => {
+  return await fetch(SERVER_URL + "/", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ id }),
+  })
+    .then(handleInvalidResponse)
+    .then((response) => response.json());
+};
+
+const VoteAPI = {
+  getScore,
+  insertVote,
+  removeScore,
+};
+export default VoteAPI;
