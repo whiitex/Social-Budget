53b20611da866e1b7f55c2886abb719301b54d84
whiitex <pasquale03.bianco@gmail.com>
2024-07-04 19:55:53 +0200

    feat: route control

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/socialbudget" element={<App />} />
+        <Route path="/" element={<Navigate replace to="/socialbudget" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
