6d9d66a3794353cb6f2767ee85bfd0ca00728144
whiitex <pasquale03.bianco@gmail.com>
2024-07-04 15:07:25 +0200

    add: validation helper for express-validator

---
+++ server/src/utilities.mjs
@@ -0,0 +1,18 @@
+import { validationResult } from "express-validator";
+
+/**
+ * Validates the request object and returns an error if the request object is not formatted properly
+ * @param req - The request object
+ * @param res - The response object
+ * @param next - The next function
+ * @returns Returns the next function if there are no errors or a response with a status code of 422 if there are errors.
+ */
+const validateRequest = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = "The parameters are not formatted properly";
+    return res.status(422).json({ message: error });
+  } else return next();
+};
+
+export default validateRequest;
