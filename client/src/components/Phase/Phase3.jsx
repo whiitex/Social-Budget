169a3bb3a8429be2fcc12dea33852fa599322db8
whiitex <pasquale03.bianco@gmail.com>
2024-07-05 17:06:54 +0200

    fix: minor aesthetic bugs

--- client/src/components/Phase/Phase3.jsx
+++ client/src/components/Phase/Phase3.jsx
@@ -6,23 +6,29 @@ const Phase3 = ({ proposals, budget, user }) => {
   return (
     <>
       {/* Approved */}
-      <div id="final" className="row mb-2">
-        <h3 className="text-center mt-5">Approved proposals</h3>
-      </div>
-      <div id="final" className="row mb-5">
-        {proposals
-          .filter((p) => p.isapproved)
-          .map((proposal, index) => (
-            <Proposal3
-              key={proposal.id}
-              proposal={proposal}
-              order={index + 1}
-            />
-          ))}
-      </div>
+      {proposals.filter((p) => p.isapproved).length == 0 ? (
+        <h3 className="text-center mt-5">No approved proposals...</h3>
+      ) : (
+        <>
+          <div id="final" className="row mb-2">
+            <h3 className="text-center mt-5">Approved proposals</h3>
+          </div>
+          <div id="final" className="row mb-5">
+            {proposals
+              .filter((p) => p.isapproved)
+              .map((proposal, index) => (
+                <Proposal3
+                  key={proposal.id}
+                  proposal={proposal}
+                  order={index + 1}
+                />
+              ))}
+          </div>
+        </>
+      )}
 
       {/* Non approved */}
-      {user ? (
+      {user && proposals.filter((p) => !p.isapproved).length > 0 ? (
         <>
           <div id="final" className="row mb-2">
             <h3 className="text-center mt-5">Non approved proposals</h3>
