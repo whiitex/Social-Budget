c4cb5580e195efcf6d94dda90790d2bee9f88b95
whiitex <pasquale03.bianco@gmail.com>
2024-07-19 15:09:31 +0200

    final

--- client/src/components/Proposal/Proposal3.jsx
+++ client/src/components/Proposal/Proposal3.jsx
@@ -5,7 +5,11 @@ const Proposal3 = ({ proposal, order }) => {
   return (
     <div className="col-md-4 col-sm-6 col-6">
       <div className={"box" + (proposal.isapproved ? " approved" : "")}>
-        <i className="bi bi-file-check verified-icon mr-2 mt-2"></i>
+        {proposal.isapproved ? (
+          <i className="bi bi-file-check verified-icon mr-2 mt-2"></i>
+        ) : (
+          <></>
+        )}
         <h4># {order}</h4>
         <p className="box-description">{proposal.description}</p>
         <p>
